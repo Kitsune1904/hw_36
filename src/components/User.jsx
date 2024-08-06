b4927c7a3ebc6f8b2feba9a76f65237ab8dc4748
Kitsune1904 <annagumiho190423@gmail.com>
2024-08-06 15:02:19 +0300

    changed eff user

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,29 +1,32 @@
 import { useEffect, useState } from "react";
 
 const User = () => {
-  const [user, setUser] = useState(null);
+  const [userData, setUserData] = useState(null);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users/1")
-      .then((response) =>response.json()
-          .then((userData) => {
-            setUser(userData);
-          })
-          .catch((error) => console.error(error))
-      )
-      .catch((error) => console.error(error))
-  }, []);
+    const fetchData = async () => {
+        try {
+            const response = await fetch("https://jsonplaceholder.typicode.com/users/1");
+            if(!response.ok) {
+                throw new Error("Problem with internet conection");
+            }
+            const result = await response.json();
+            setUserData(result);
+        } catch(err) {
+            setError(err.message)
+        }
+    }
+    fetchData();
+  }, [])
 
-  console.log(user)
-
-    if(user) {
-        return Object.keys(user).length >= 1 ? (
-            <div className="userInfo">
-                <p><span>User name is - </span>{user.name}</p>
-            </div>) :
-            <p>No data recived</p>
-    } else {
-        return <p>Loading...</p>
-    }    
+  return (
+    <>
+    {error && <p>Error: {error}</p>}
+    {userData ? <div className="userInfo">
+                <p><span>User name is - </span>{userData.name}</p>
+            </div> : <p>No data recived</p>}
+    </>
+  )
 };
 export default User;
