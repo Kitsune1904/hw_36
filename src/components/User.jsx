64fdfdb32e9596a64643a22f948301ff78ec6cd4
Kitsune1904 <annagumiho190423@gmail.com>
2024-08-07 12:24:32 +0300

    changed err shwing

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -6,30 +6,36 @@ const User = () => {
 
   useEffect(() => {
     const fetchData = async () => {
-        try {
-            const response = await fetch("https://jsonplaceholder.typicode.com/users/1");
-            if(!response.ok) {
-                throw new Error("Problem with internet conection");
-            }
-            const result = await response.json();
-            setUserData(result);
-            setError(null)
-        } catch(err) {
-            console.error(err)
-            setError(err.message)
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/users/1"
+        );
+        if (!response.ok) {
+          throw new Error("Problem with internet conection");
         }
-    }
+        const result = await response.json();
+        setUserData(result);
+        setError(null);
+      } catch (err) {
+        console.error(err);
+        setError(err.message);
+      }
+    };
     fetchData();
-  }, [])
+  }, []);
 
   return (
     <>
-    {error && <p>Error - {error}</p> }
-    {userData ? <div className="userInfo">
-                <p><span>User name is - </span>{userData.name}</p>
-            </div> : <p>Loading...</p>}
+      {error 
+        ? <p>Error - {error}</p>
+        :userData 
+          ? <div className="userInfo">
+              <p><span>User name is - </span>{userData.name}</p>
+            </div>
+          : <p>Loading...</p>
+      }
     </>
-  )
+  );
 };
 
 export default User;
